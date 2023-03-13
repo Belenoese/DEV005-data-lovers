0b05d07fb0cf9ef422c2422a0ad666613da67887
Belen Neira <belen.neira05@gmail.com>
2023-03-13 16:15:38 -0300

    avance codigo buscar por nombre

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import { tarjetas } from './data.js';
-//import pokemon from './data/pokemon/pokemon.js';
-// import data from './data/lol/lol.js';
+import pokemon from './data/pokemon/pokemon.js';
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 const bloqueTarjetas = document.getElementById("bloqueTarjetas");
 bloqueTarjetas.innerHTML= tarjetas(data);
+
+const ordenarInput = document.getElementById("name");
+ordenarInput.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        const busqueda = event.target.value.toLowerCase();
+        const pokemonFiltrados = data.pokemon.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(busqueda));
+
+        bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonFiltrados});
+        console.log(bloqueTarjetas);
+    }
+});
 
 
 const filtroTipo=document.getElementById('filtroTipo');
