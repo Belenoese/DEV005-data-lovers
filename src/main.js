ea20d952678be40bdf264bf261ab64f9931ac571
mmayalae <mmayalae@pichincha.com>
2023-03-14 09:32:45 -0500

    se extrae data para reverso de tarjeta

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { tarjetas } from './data.js';
+
 import pokemon from './data/pokemon/pokemon.js';
 
 import data from './data/pokemon/pokemon.js';
@@ -6,16 +7,18 @@ import data from './data/pokemon/pokemon.js';
 const bloqueTarjetas = document.getElementById("bloqueTarjetas");
 bloqueTarjetas.innerHTML= tarjetas(data);
 
-const ordenarInput = document.getElementById("name");
-ordenarInput.addEventListener("keyup", (event) => {
-    if (event.key === "Enter") {
-        const busqueda = event.target.value.toLowerCase();
-        const pokemonFiltrados = data.pokemon.filter((pokemon) =>
-        pokemon.name.toLowerCase().includes(busqueda));
+bloqueTarjetas.innerHTML = tarjetas (pokemon.stats);
 
-        bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonFiltrados});
-        console.log(bloqueTarjetas);
-    }
+const ordenarInput = document.getElementById("name");
+ordenarInput.addEventListener("input", (event) => {
+ 
+  const busqueda = event.target.value.toLowerCase();
+  const pokemonFiltrados = data.pokemon.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(busqueda));
+
+  bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonFiltrados});
+  console.log(bloqueTarjetas);
+  
 });
 
 
