5dc7ced5b7cdcdf8d8de02f0a16d2a92eea4c460
Belen Neira <belen.neira05@gmail.com>
2023-03-16 13:58:37 -0300

    actualizacion de data manual

--- src/data.js
+++ src/data.js
@@ -1,32 +1,26 @@
-// estas funciones son de ejemplo
-
-
 export const tarjetas = (data) => {
   let tarjetaString = " ";
   for (const pokemon of data.pokemon){
     tarjetaString +=`
-    <div class= "tarjetapokemon"> 
+    <div class= "tarjetapokemon">
     <div class= "tarjeta">
-    <div class= "frontTarjeta"> <h1> <p> #${pokemon.stats}</h1> </p> </div>
-    <div class= "backTarjeta">
+    <div class= "frontTarjeta">   
     <img class= "imagenPokemon" src = ${pokemon.img}>
     <p> #${pokemon.num} <br> ${pokemon.name.toUpperCase()} <br>Tipo: ${pokemon.type}</p>
     </div>
-    </div>
+    <div class= "backTarjeta">
+    <p> Ataque: <br> ${pokemon.stats["base-attack"]} 
+    <br> Defensa: <br> ${pokemon.stats["base-defense"]} 
+    <br> Resistencia: <br> ${pokemon.stats["base-stamina"]}
+    <br> Max-cp: <br> ${pokemon.stats["max-cp"]}
+    <br> Max-hp: <br> ${pokemon.stats["base-defense"]}</p>
+    </div>  
+    </div> 
     </div>`
-    
-    
+    //console.log(pokemon.stats);
   }
 
   return tarjetaString;
   
 
-};
-
-
-
-
-
-export const anotherExample = () => {
-  return 'OMG';
 };
