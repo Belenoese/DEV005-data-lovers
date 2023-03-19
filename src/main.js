13ad7bb30d2fd7434da258956d5766162f45351d
Belen Neira <belen.neira05@gmail.com>
2023-03-19 19:17:07 -0300

    implementando funcion para orden alfabetico

--- src/main.js
+++ src/main.js
@@ -1,31 +1,43 @@
-import { tarjetas, filtrarTipos } from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
+import { tarjetas, filtrarTipos, ordenarPorNombre } from "./data.js";
+import pokemon from "./data/pokemon/pokemon.js";
 
-import data from './data/pokemon/pokemon.js';
+import data from "./data/pokemon/pokemon.js";
 
 //Mostrar las tarjetas en la pagina
 const bloqueTarjetas = document.getElementById("bloqueTarjetas");
-bloqueTarjetas.innerHTML= tarjetas(data);
+bloqueTarjetas.innerHTML = tarjetas(data);
 
 //Buscar Pokemon por nombre
 const ordenarInput = document.getElementById("name");
 ordenarInput.addEventListener("input", (event) => {
- 
   const busqueda = event.target.value.toLowerCase();
   const pokemonFiltrados = data.pokemon.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(busqueda));
-  bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonFiltrados});  
+    pokemon.name.toLowerCase().includes(busqueda)
+  );
+  bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonFiltrados });
 });
 
 //Filtrado por tipo de Pokemon
 const filtroTipo = document.getElementById("filtroTipo");
-filtroTipo.addEventListener('change', function(e) {
-
+filtroTipo.addEventListener("change", function (e) {
   const tipoSeleccionado = e.target.value;
   const pokemonHTML = data.pokemon.filter((pokemon) =>
-    pokemon.type.includes(tipoSeleccionado));
+    pokemon.type.includes(tipoSeleccionado)
+  );
   bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonHTML });
   //console.log('Cambio el tipo por: ',pokemonHTML);
-  
 });
 
+const ordenarSelect = document.getElementById("ordenar");
+ordenarSelect.addEventListener("change", () => {
+  const pokemonOrdenados = data.pokemon.slice().sort((a, b) => {
+    if (ordenarSelect.value === "asc") {
+      return ordenarPorNombre(a, b);
+    } else if (ordenarSelect.value === "desc") {
+      return ordenarPorNombre(b, a);
+    } else {
+      return 0;
+    }
+  });
+  bloqueTarjetas.innerHTML = tarjetas({ pokemon: pokemonOrdenados });
+});
